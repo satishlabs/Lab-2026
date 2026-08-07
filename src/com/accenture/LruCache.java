6dea860aca6afb953077d9898ebb42829332f583
satishlabs <spd61185@gmail.com>
2026-08-07 16:13:30 +0530

    Added some code

---
+++ src/com/accenture/LruCache.java
@@ -0,0 +1,17 @@
+package com.accenture;
+
+public class LruCache {
+    class ListNode{
+        int data;
+        ListNode next;
+
+        public ListNode(int data){
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
--- src/com/accenture/RemoveDuplicateInList.java
+++ src/com/accenture/RemoveDuplicateInList.java
@@ -1,4 +1,4 @@
-package com.dsa.list;
+package com.accenture;
 
 import java.util.*;
 
